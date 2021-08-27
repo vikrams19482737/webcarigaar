7177ee59138df8510a2f17f09c4810428114cef9
vikram <vikram.s8541@gmail.com>
2021-08-27 01:35:30 -0700

    testing commit

--- src/components/home.js
+++ src/components/home.js
@@ -12,11 +12,6 @@ function Home(){
   return(
          <div className='abc'>
                 <Navbar />
-                <Section1 />
-                <Section2 />
-                <Section3 />
-                <Section4 />
-                <Footer />
          </div>)
 }
 

3c2119b322babe410adc992be56468a2ff4a276b
vikram <vikram.s8541@gmail.com>
2021-08-27 01:47:51 -0700

    test2

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,6 @@
 import React from 'react-dom';
-
 import Navbar   from './navbar';
-import Section1 from './section1';
-import Section2 from './section2';
-import Section3 from './section3';
-import Section4 from './section4';
-import Footer   from './footer';
+
 
 function Home(){
 

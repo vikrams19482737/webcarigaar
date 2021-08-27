5555c21aa3ceeee972a4cd362f34481a89b2f6b2
vikram <vikram.s8541@gmail.com>
2021-08-27 03:09:55 -0700

    footer

--- src/components/home.js
+++ src/components/home.js
@@ -4,7 +4,7 @@ import Navbar   from './navbar';
 import Section1 from './section1';
 import Section2 from './section2';
 import Section3 from './section3';
-
+import Footer from './footer';
 
 function Home(){
 
@@ -14,6 +14,7 @@ function Home(){
                 <Section1 />
                 <Section2 />
                 <Section3 />
+                <Footer />
          </div>)
 }
 

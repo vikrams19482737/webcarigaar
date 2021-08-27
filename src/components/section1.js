0d960de7f1c8505184914083c6bbda15561848c8
vikram <vikram.s8541@gmail.com>
2021-08-27 02:38:55 -0700

    added image

--- src/components/section1.js
+++ src/components/section1.js
@@ -1,10 +1,12 @@
 import React from 'react-dom'
+import cimg2 from './cimg2.svg';
 
 
 function Section1(){
   return (
        <div className='section1_container'>
               <div> Just a text </div>
+               <img src={cimg2}  alt="logo" />
         </div>
      )
 }

f58532741d157bc93fbbe36cbbfdb856c98248d6
vikram <vikram.s8541@gmail.com>
2021-08-27 02:56:09 -0700

    test all

--- src/components/home.js
+++ src/components/home.js
@@ -2,6 +2,8 @@ import React from 'react-dom';
 
 import Navbar   from './navbar';
 import Section1 from './section1';
+import Section2 from './section2';
+import Section3 from './section3';
 
 
 function Home(){
@@ -10,6 +12,8 @@ function Home(){
          <div className='abc'>
                 <Navbar />
                 <Section1 />
+                <Section2 />
+                <Section3 />
          </div>)
 }
 
